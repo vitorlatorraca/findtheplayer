0fb54a7f3c5fff4f18cabdc9979f8c93d658e951
vitorlatorraca <vitorlatorraca5@gmail.com>
2025-01-21 22:29:31 -0500

    new

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(express.json());
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'RafaelLeao17.',
+    password: '',
     database: 'game1',
     port: '3306'
 });
